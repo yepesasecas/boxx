797d8c0ea3529f7d7b5767b6b808cab5fb6a0004
Andres Yepes <andresyepes@Andress-MacBook-Air.local>
2013-12-17 19:39:23 -0500

    porfin

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,2 +1,24 @@
+function valor_mensual(valor){
+	var monto = "$"+valor*20000+ " pesos";
+	$(".valor_solicitud").text(monto);
+}
+function sumar_caja(){
+	var input_cajas = $("#input_numero_cajas");
+	valor = input_cajas.val();
+	valor++;
+	input_cajas.val(valor);
+	valor_mensual(valor);
+}
+function restar_caja(){
+	var input_cajas = $("#input_numero_cajas");
+	valor = input_cajas.val();
+	valor--;
+	input_cajas.val(valor);
+	valor_mensual(valor);
+}
 
+$(document).ready(function(){
+	$("#sumar_caja").on("click", sumar_caja);
+	$("#restar_caja").on("click", restar_caja);
+});	
 	
